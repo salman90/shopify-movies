490bc90a6ce889fe17118fb045d69c346d1b0e87
salman90 <salmansalem9022@gmail.com>
2020-09-06 20:42:59 -0700

    Updates

---
+++ precache-manifest.78d8f5db8d217b8beb8bb048cba98067.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a70c6213ccb612500c3e7edcbb38a929",
+    "url": "/%7Bshopify-movie%7D/index.html"
+  },
+  {
+    "revision": "e847769d5227c0758653",
+    "url": "/%7Bshopify-movie%7D/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "15af66292a4960ea2a5d",
+    "url": "/%7Bshopify-movie%7D/static/css/main.6503cd60.chunk.css"
+  },
+  {
+    "revision": "e847769d5227c0758653",
+    "url": "/%7Bshopify-movie%7D/static/js/2.23cfb983.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/%7Bshopify-movie%7D/static/js/2.23cfb983.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15af66292a4960ea2a5d",
+    "url": "/%7Bshopify-movie%7D/static/js/main.9e35e51a.chunk.js"
+  },
+  {
+    "revision": "8ae1050cbaf296c51608",
+    "url": "/%7Bshopify-movie%7D/static/js/runtime-main.a468380a.js"
+  },
+  {
+    "revision": "c3d52d90ef78fc63dd9e9421d8a30f40",
+    "url": "/%7Bshopify-movie%7D/static/media/shopifyLogo.c3d52d90.png"
+  }
+]);
