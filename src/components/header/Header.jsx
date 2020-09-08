0327fd22093b76aaf3b1c0373f3e51d311288907
salman90 <salmansalem9022@gmail.com>
2020-09-08 07:50:32 -0700

    fixed file name header issue

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styles from './Header.module.css';
-import Logo from '../Logo'
+import Logo from '../logo'
 import imageSrc from '../../images/shopifyLogo.png';
 
 
