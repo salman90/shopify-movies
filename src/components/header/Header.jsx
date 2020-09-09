f3805a2b2da96825a42dd544db7a2af2c5e1fa4e
salman90 <salmansalem9022@gmail.com>
2020-09-09 11:05:14 -0700

    fixed import of logo to match index.js

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styles from './Header.module.css';
-import Logo from '../logo'
+import Logo from '../Logo'
 import imageSrc from '../../images/shopifyLogo.png';
 
 
